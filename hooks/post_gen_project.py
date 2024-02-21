787b2189c4ff4380bf5d991210f260f36263d4f4
Ajay Bhargava <bhargava.ajay@gmail.com>
2024-02-21 01:55:26 +0000

    fix: better error handling for lack of file paths that have already been removed.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -15,85 +15,114 @@ is_publishable_package = "{{ not cookiecutter.with_fastapi_api|int and not cooki
 is_ml_training_script = int("{{ cookiecutter.with_ml_training }}")
 is_ml_inference_script = int("{{ cookiecutter.with_ml_inference }}")
 
-try:
-    # Remove py.typed and Dependabot if not in strict mode.
-    if development_environment != "strict":
+# Remove py.typed and Dependabot if not in strict mode.
+if development_environment != "strict":
+    if os.path.exists(f"src/{package_name}/py.typed"):
         os.remove(f"src/{package_name}/py.typed")
+    if os.path.exists(".github/dependabot.yml"):
         os.remove(".github/dependabot.yml")
 
-    # Remove FastAPI if not selected.
-    if not with_fastapi_api:
+# Remove FastAPI if not selected.
+if not with_fastapi_api:
+    if os.path.exists(f"src/serve/api.py"):
         os.remove(f"src/serve/api.py")
+    if os.path.exists("__tests__/test_api.py"):
         os.remove("__tests__/test_api.py")
 
-    # Remove Sentry if not selected.
-    if not with_sentry_logging:
+# Remove Sentry if not selected.
+if not with_sentry_logging:
+    if os.path.exists(f"src/{package_name}/sentry.py"):
         os.remove(f"src/{package_name}/sentry.py")
+    if os.path.exists("__tests__/test_sentry.py"):
         os.remove("__tests__/test_sentry.py")
 
-    # Remove Streamlit if not selected.
-    if not with_streamlit_app:
+# Remove Streamlit if not selected.
+if not with_streamlit_app:
+    if os.path.exists(f"src/serve/app.py"):
         os.remove(f"src/serve/app.py")
 
-    # Remove ML training scripts if not selected.
-    if not is_ml_training_script:
+# Remove ML training scripts if not selected.
+if not is_ml_training_script:
+    if os.path.exists(f"src/{package_name}/fit.py"):
         os.remove(f"src/{package_name}/fit.py")
+    if os.path.exists("__tests__/test_train.py"):
         os.remove("__tests__/test_train.py")
+    if os.path.exists(".github/workflows/train.yml"):
         os.remove(".github/workflows/train.yml")
+    if os.path.exists(f"src/{package_name}/train"):
         shutil.rmtree(f"src/{package_name}/train")
 
-    # Remove ML inference scripts if not selected.
-    if not is_ml_inference_script:
+# Remove ML inference scripts if not selected.
+if not is_ml_inference_script:
+    if os.path.exists(f"src/{package_name}/deploy.py"):
         os.remove(f"src/{package_name}/deploy.py")
+    if os.path.exists(".github/workflows/endpoint.yml"):
         os.remove(".github/workflows/endpoint.yml")
+    if os.path.exists("src/serve/package.json"):
         os.remove("src/serve/package.json")
+    if os.path.exists("src/serve/requirements.txt"):
         os.remove("src/serve/requirements.txt")
+    if os.path.exists("src/serve/serverless.yml"):
         os.remove("src/serve/serverless.yml")
-        os.remove(f"src/{package_name}/deploy-inference.py")
+    if os.path.exists(f"src/{package_name}/deploy"):
         shutil.rmtree(f"src/{package_name}/deploy")
 
-    # Neither ML training nor inference is selected.
-    if not is_ml_training_script and not is_ml_inference_script:
+# Neither ML training nor inference is selected.
+if not is_ml_training_script and not is_ml_inference_script:
+    if os.path.exists(f"src/{package_name}/settings.py"):
         os.remove(f"src/{package_name}/settings.py")
+    if os.path.exists(".github/workflows/serve.yml"):
         os.remove(".github/workflows/serve.yml")
 
-    # Remove Typer if not selected.
-    if not with_typer_cli:
+# Remove Typer if not selected.
+if not with_typer_cli:
+    if os.path.exists(f"src/{package_name}/cli.py"):
         os.remove(f"src/{package_name}/cli.py")
+    if os.path.exists("__tests__/test_cli.py"):
         os.remove("__tests__/test_cli.py")
 
-    # Remove Serve Directory if neither FastAPI nor Streamlit is selected.
-    if not with_fastapi_api and not with_streamlit_app:
+# Remove Serve Directory if neither FastAPI nor Streamlit is selected.
+if not with_fastapi_api and not with_streamlit_app:
+    if os.path.exists(".github/workflows/serve.yml"):
         os.remove(".github/workflows/serve.yml")
+    if os.path.exists("src/serve"):
         shutil.rmtree("src/serve")
 
-    if is_ml_inference_script and is_api_endpoint:
+if is_ml_inference_script and is_api_endpoint:
+    if os.path.exists(".github/workflows/ship.yml"):
         os.remove(".github/workflows/ship.yml")
+    if os.path.exists("src/serve/api.py"):
         os.remove("src/serve/api.py")
         os.rename("src/serve/api-ml-inference.py", "src/serve/api.py")
 
-    if is_ml_inference_script and is_api_endpoint and not is_ml_training_script:
+if is_ml_inference_script and is_api_endpoint and not is_ml_training_script:
+    if os.path.exists(f"src/{package_name}/deploy"):
         shutil.rmtree(f"src/{package_name}/deploy")
-        os.remove(f"src/{package_name}/deploy.py")
+    if os.path.exists(f"src/{package_name}/deploy-inference.py"):
+        os.remove(f"src/{package_name}/deploy-inference.py")
         os.rename(f"src/{package_name}/deploy-inference.py", f"src/{package_name}/deploy.py")
 
-    if is_api_endpoint and not is_ml_inference_script:
+if is_api_endpoint and not is_ml_inference_script:
+    if os.path.exists("src/serve/api-ml-inference.py"):
         os.remove("src/serve/api-ml-inference.py")
 
-    if is_ml_training_script and is_ml_inference_script:
+if is_ml_training_script and is_ml_inference_script:
+    if os.path.exists(f"src/{package_name}/deploy-inference.py"):
         os.remove(f"src/{package_name}/deploy-inference.py")
 
-    # Remove the continuous integration provider that is not selected.
-    if continuous_integration != "GitHub":
+# Remove the continuous integration provider that is not selected.
+if continuous_integration != "GitHub":
+    if os.path.exists(".github/"):
         shutil.rmtree(".github/")
-    elif continuous_integration != "GitLab":
+elif continuous_integration != "GitLab":
+    if os.path.exists(".gitlab-ci.yml"):
         os.remove(".gitlab-ci.yml")
 
-    # Remove unused GitHub Actions workflows.
-    if continuous_integration == "GitHub":
-        if not is_deployable_app and not is_api_endpoint:
+# Remove unused GitHub Actions workflows.
+if continuous_integration == "GitHub":
+    if not is_deployable_app and not is_api_endpoint:
+        if os.path.exists(".github/workflows/ship.yml"):
             os.remove(".github/workflows/ship.yml")
-        if not is_publishable_package:
+    if not is_publishable_package:
+        if os.path.exists(".github/workflows/publish.yml"):
             os.remove(".github/workflows/publish.yml")
-except FileNotFoundError:
-    pass
